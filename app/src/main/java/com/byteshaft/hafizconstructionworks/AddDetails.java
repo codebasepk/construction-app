792dcdefc1679eccad114835e29d2ed669439782
Shahid Ghafoor <shgr8rb@gmail.com>
2018-09-17 22:52:51 +0500

    init project

---
+++ app/src/main/java/com/byteshaft/hafizconstructionworks/AddDetails.java
@@ -0,0 +1,14 @@
+package com.byteshaft.hafizconstructionworks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("Add Detail");
+        setContentView(R.layout.activity_add_details);
+    }
+}
