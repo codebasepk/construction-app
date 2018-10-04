3b02ed7704d5847d685daec923cd7bd057a2d1c8
shahid ghafoor <shahidghafoor00@gmail.com>
2018-10-04 14:39:15 +0500

    add dialog for add quarter and http methods

--- app/src/main/java/com/byteshaft/hafizconstructionworks/MainActivity.java
+++ app/src/main/java/com/byteshaft/hafizconstructionworks/MainActivity.java
@@ -1,5 +1,8 @@
 package com.byteshaft.hafizconstructionworks;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,21 +11,40 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.byteshaft.requests.HttpRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.HttpURLConnection;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView mListView;
+    private QuarterAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        mListView = findViewById(R.id.quarter_list);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, AddDetails.class));
+                addQuarterDialog();
 //                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
 //                        .setAction("Action", null).show();
             }
@@ -49,5 +71,127 @@ public class MainActivity extends AppCompatActivity {
 //        }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void addQurter(String quarterName) {
+        HttpRequest request = new HttpRequest(getApplicationContext());
+        request.setOnReadyStateChangeListener(new HttpRequest.OnReadyStateChangeListener() {
+            @Override
+            public void onReadyStateChange(HttpRequest request, int readyState) {
+                switch (readyState) {
+                    case HttpRequest.STATE_DONE:
+                        switch (request.getStatus()) {
+                            case HttpURLConnection.HTTP_OK:
+                        }
+                }
+            }
+        });
+        request.setOnErrorListener(new HttpRequest.OnErrorListener() {
+            @Override
+            public void onError(HttpRequest httpRequest, int i, short i1, Exception e) {
+
+            }
+        });
+        request.open("POST", String.format("%saddkjlkn/", "Url"));
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("name", quarterName);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        request.send(jsonObject.toString());
+    }
+
+    private void getAllQuarters() {
+        HttpRequest request = new HttpRequest(getApplicationContext());
+        request.setOnReadyStateChangeListener(new HttpRequest.OnReadyStateChangeListener() {
+            @Override
+            public void onReadyStateChange(HttpRequest httpRequest, int i) {
+
+            }
+        });
+        request.setOnErrorListener(new HttpRequest.OnErrorListener() {
+            @Override
+            public void onError(HttpRequest httpRequest, int i, short i1, Exception e) {
+
+            }
+        });
+        request.open("GET", "");
+        request.send();
+    }
+
+    private void addQuarterDialog() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
+        alertDialog.setTitle("Add Quarter");
+        alertDialog.setMessage("");
+        FrameLayout container = new FrameLayout(MainActivity.this);
+        final EditText input = (EditText) getLayoutInflater().inflate(R.layout.dialog_editext, null);
+        FrameLayout.LayoutParams lp = new FrameLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+        lp.leftMargin = getResources().getDimensionPixelSize(R.dimen.dialog_margin);
+        lp.rightMargin = getResources().getDimensionPixelSize(R.dimen.dialog_margin);
+        lp.bottomMargin = getResources().getDimensionPixelSize(R.dimen.dialog_margin);
+        container.addView(input);
+        input.setLayoutParams(lp);
+        alertDialog.setView(container);
+        alertDialog.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                addQurter(input.getText().toString());
+            }
+        });
+        alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        alertDialog.show();
+    }
+
+    private class QuarterAdapter extends BaseAdapter {
+
+        private ViewHolder viewHolder;
+        private Context context;
+
+        private QuarterAdapter(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = getLayoutInflater()
+                        .inflate(R.layout.delegate_quarter, parent, false);
+                viewHolder = new ViewHolder();
+                viewHolder.quarterTitle = convertView.findViewById(R.id.text_view_wish_list_title);
+                convertView.setTag(viewHolder);
+            } else {
+                viewHolder = (ViewHolder) convertView.getTag();
+            }
+
+            return convertView;
+        }
+
+        private class ViewHolder {
+            private TextView quarterTitle;
+        }
     }
 }
